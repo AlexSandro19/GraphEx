92caa9929e6796bf95e82ac8d0a2f8a15abde166
AlexSandro19 <a.sandrovschii@gmail.com>
2023-05-22 23:47:17 +0200

    added redis_connection to all functions

--- graph_ex/lib/graph_ex/relationship.ex
+++ graph_ex/lib/graph_ex/relationship.ex
@@ -8,45 +8,45 @@ defmodule GraphEx.Relationship do
     struct(__MODULE__, map)
   end
 
-  def add_relationship(graph, relationship) do
+  def add_relationship(connection, graph, relationship) do
     inner_query = "MATCH (n#{Utils.labels_to_string(relationship.src_node.labels)} #{Utils.properties_to_string(relationship.src_node.properties)}),
                          (m#{Utils.labels_to_string(relationship.dest_node.labels)} #{Utils.properties_to_string(relationship.dest_node.properties)})
                    CREATE (n)-[r#{Utils.type_to_string(relationship.type)} #{Utils.properties_to_string(relationship.properties)}]->(m)
                    RETURN r"
     query = ["GRAPH.QUERY", graph.name, inner_query]
-    {:ok, res} = Redix.command(graph.connection, query) # decide what to do with res
+    {:ok, res} = Redix.command(connection, query) # decide what to do with res
     {:ok, relationship_array } = extract_data(res)
     {:ok, List.first(relationship_array)}
     # {:ok, relationship}
   end
 
-  def get_relationships(graph, src_node \\ %GraphEx.Node{}, type \\ "", properties \\ %{}, dest_node \\ %GraphEx.Node{})  do # %Node{} -- doesn't work for some reason;
+  def get_relationships(connection, graph, src_node \\ %GraphEx.Node{}, type \\ "", properties \\ %{}, dest_node \\ %GraphEx.Node{})  do # %Node{} -- doesn't work for some reason;
     # need to think about the order of the parameters again, and how to better do it
     IO.inspect(dest_node)
-    if is_pid(graph.connection) do
+    if is_pid(connection) do
       src_node_inner_query = "(n#{Utils.labels_to_string(src_node.labels)} #{Utils.properties_to_string(src_node.properties)})"
       relationship_inner_query = "[r#{Utils.type_to_string(type)} #{Utils.properties_to_string(properties)}]"
       dest_node_inner_query = "(m#{Utils.labels_to_string(dest_node.labels)} #{Utils.properties_to_string(dest_node.properties)})"
       inner_query = "MATCH #{src_node_inner_query}-#{relationship_inner_query}->#{dest_node_inner_query} RETURN r"
 
       query = ["GRAPH.QUERY", graph.name, inner_query]
-      {:ok, res} = Redix.command(graph.connection, query)
+      {:ok, res} = Redix.command(connection, query)
       extract_data(res)
     end
   end
 
-  def update_relationship(graph, upd_relationship) do # can I let people change the type, src_node and dest_node? and how to enforce it otherwise (through const)?
+  def update_relationship(connection, graph, upd_relationship) do # can I let people change the type, src_node and dest_node? and how to enforce it otherwise (through const)?
     inner_query = "MATCH (n)-[r]->(m) WHERE id(r) = #{upd_relationship.id} SET r = #{Utils.properties_to_string(upd_relationship.properties)} RETURN r"
     query = ["GRAPH.QUERY", graph.name, inner_query]
-    {:ok, res} = Redix.command(graph.connection, query)
+    {:ok, res} = Redix.command(connection, query)
     {:ok, relationship_array } = extract_data(res)
     {:ok, List.first(relationship_array)}
   end
 
-  def delete_relationship(graph, relationship_to_delete) do
+  def delete_relationship(connection, graph, relationship_to_delete) do
     inner_query = "MATCH (n)-[r]->(m) WHERE id(r) = #{relationship_to_delete.id} DELETE r"
     query = ["GRAPH.QUERY", graph.name, inner_query]
-    {:ok, _res} = Redix.command(graph.connection, query)
+    {:ok, _res} = Redix.command(connection, query)
     {:ok, relationship_to_delete}
   end
 
